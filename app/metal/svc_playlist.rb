fa86a0030ba9476cc92856b296eecb8452650f91
Robb Broome <robb@teamvoice.com>
2009-10-16 00:30:02 -0500

    billion loader

--- app/metal/svc_playlist.rb
+++ app/metal/svc_playlist.rb
@@ -5,7 +5,17 @@ class SvcPlaylist
   def self.call(env)
     if env["PATH_INFO"] =~ /^\/playlist/
       p = Playlist.all().first
-      [200, {"Content-Type" => "text/html"}, [p.to_json]]
+      [200, {"Content-Type" => "text/htmlG"}, [p.to_json]]
+    elsif env["PATH_INFO"] =~ /^\/make/
+      
+      for n in 1..1000000000
+        p = Playlist.new(:title => "playlist load " + n.to_s, :user_id => rand(10000), :description => "Created at " + Time.now.to_s)
+        items = rand(20) + 3
+        for i in 1..items
+          p.playlist_items << PlaylistItem.new(:item_id => rand(100000000), :item_type_id => 1, :position => i)
+        end 
+        p.save
+      end 
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
