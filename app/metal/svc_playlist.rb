abb1ac0eef19cc713cd5f4247496ed5f5865866b
Robb Broome <robb@teamvoice.com>
2009-10-16 10:46:20 -0500

    add a test action with no db access

--- app/metal/svc_playlist.rb
+++ app/metal/svc_playlist.rb
@@ -6,15 +6,19 @@ class SvcPlaylist
     if env["PATH_INFO"] =~ /^\/playlist/
       p = Playlist.all().first
       [200, {"Content-Type" => "text/htmlG"}, [p.to_json]]
+    elsif env["PATH_INFO"] =~ /^\/test/
+      [200, {"Content-Type" => "text/html"}, ['tested']]
     elsif env["PATH_INFO"] =~ /^\/make/
-        n = rand(100000000000)
+      for n in 1..100000000
+        # n = rand(100000000000)
         p = Playlist.new(:title => "playlist load " + n.to_s, :user_id => rand(10000), :description => "Created at " + Time.now.to_s)
         p.save
-        items = rand(5) + 1
+        items = rand(50) + 10
         for i in 1..items
           p.playlist_items << PlaylistItem.new(:item_id => rand(100000000), :item_type_id => 1, :position => i)
           p.save
         end 
+      end
         [200, {"Content-Type" => "text/html"}, ['done']]
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
