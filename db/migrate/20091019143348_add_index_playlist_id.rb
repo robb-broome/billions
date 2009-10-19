2126a6b1b9cd37eaee087b5c9c2f498bee40ada7
Robb Broome <robb@teamvoice.com>
2009-10-19 09:42:14 -0500

    remove duplicate migration

--- db/migrate/20091019143348_add_index_playlist_id.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexPlaylistId < ActiveRecord::Migration
-  def self.up
-    add_index :playlist_items, :playlist_id
-  end
-
-  def self.down
-    remove_index :playlist_items, :playlist_id
-  end
-end
