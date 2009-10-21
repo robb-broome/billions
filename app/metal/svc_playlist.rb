7893cae0405e996d58167da0bd2923a19a853ce3
Robb Broome <robb@teamvoice.com>
2009-10-21 16:22:31 -0500

    confirming operation

--- app/metal/svc_playlist.rb
+++ app/metal/svc_playlist.rb
@@ -5,12 +5,12 @@ class SvcPlaylist
   def self.call(env)
   
     if env["PATH_INFO"] =~ /^\/test/
-      
-         pl_items = "[{\"playlist_item\":{\"item_id\":77851204,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079817,\"user_id\":8,\"position\":1,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":35251566,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079819,\"user_id\":8,\"position\":2,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":32263871,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079822,\"user_id\":8,\"position\":3,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":15351566,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079825,\"user_id\":8,\"position\":4,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":88942541,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079828,\"user_id\":8,\"position\":5,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}}]"
-
-
-      sample_playlist = "{\"playlist\":{\"status\":true,\"flags\":0,\"updated_at\":\"2009-10-17T01:19:14Z\",\"favorite_count\":0,\"title\":\"playlist load 78661392779\",\"id\":100010,\"description\":\"Created at Fri Oct 16 18:19:14 -0700 2009\",\"user_id\":838,\"created_at\":\"2009-10-17T01:19:14Z\", \"random\":\"1\", \"playlist_items\":#{pl_items}   }   }"
-      [200, {"Content-Type" => "text/html"}, [sample_playlist]]
+      # 
+      #    pl_items = "[{\"playlist_item\":{\"item_id\":77851204,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079817,\"user_id\":8,\"position\":1,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":35251566,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079819,\"user_id\":8,\"position\":2,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":32263871,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079822,\"user_id\":8,\"position\":3,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":15351566,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079825,\"user_id\":8,\"position\":4,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}},{\"playlist_item\":{\"item_id\":88942541,\"updated_at\":\"2009-10-18T19:13:05Z\",\"playlist_id\":1119191,\"id\":5079828,\"user_id\":8,\"position\":5,\"item_type_id\":1,\"created_at\":\"2009-10-18T19:13:05Z\"}}]"
+      # 
+      # 
+      # sample_playlist = "{\"playlist\":{\"status\":true,\"flags\":0,\"updated_at\":\"2009-10-17T01:19:14Z\",\"favorite_count\":0,\"title\":\"playlist load 78661392779\",\"id\":100010,\"description\":\"Created at Fri Oct 16 18:19:14 -0700 2009\",\"user_id\":838,\"created_at\":\"2009-10-17T01:19:14Z\", \"random\":\"1\", \"playlist_items\":#{pl_items}   }   }"
+      [200, {"Content-Type" => "text/html"}, ['sample_playlist']]
       
     elsif env["PATH_INFO"] =~ /^\/header/
       n = rand(100000000000)
